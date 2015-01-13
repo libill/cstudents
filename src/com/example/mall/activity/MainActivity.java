84f2718b610e8d43bdb3bb461d6f2f6875599136
libill <770737185@qq.com>
2015-01-13 19:02:44 +0800

    add fragment

--- src/com/example/mall/activity/MainActivity.java
+++ src/com/example/mall/activity/MainActivity.java
@@ -1,38 +1,284 @@
 package com.example.mall.activity;
 
-import android.app.Activity;
-import android.content.Intent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.ActionBar;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TextView;
 
 import com.example.mall.R;
+import com.example.mall.fragment.Fragment1;
+import com.example.mall.fragment.Fragment2;
+import com.example.mall.fragment.Fragment3;
+import com.example.mall.fragment.Fragment4;
+import com.example.mall.view.JazzyViewPager;
+import com.example.mall.view.JazzyViewPager.SlideCallback;
+import com.example.mall.view.JazzyViewPager.TransitionEffect;
+import com.nineoldandroids.view.ViewHelper;
 
-public class MainActivity extends Activity{
-	private String TAG="MainActivity";
+public class MainActivity extends FragmentActivity {
+	private JazzyViewPager jazzyPager;
+	List<Map<String, View>> tabViews = new ArrayList<Map<String, View>>();
+	public TabHost tabHost;
+
+	private Fragment1 mfragment1;
+	private Fragment2 mfragment2;
+	private Fragment3 mfragment3;
+	private Fragment4 mfragment4;
+	private ArrayList<Fragment> fragmentList;
+	ArrayList<String> titleList = new ArrayList<String>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		findViewById(R.id.web).setOnClickListener(new OnClickListener() {
-			
+		jazzyPager = (JazzyViewPager) findViewById(R.id.jazzyPager);
+		// --------------------
+		tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		tabHost.setup();
+		tabHost.addTab(tabHost.newTabSpec("0").setIndicator(createTab("主页", 0)).setContent(android.R.id.tabcontent));
+		tabHost.addTab(tabHost.newTabSpec("1").setIndicator(createTab("统计", 1)).setContent(android.R.id.tabcontent));
+		tabHost.addTab(tabHost.newTabSpec("2").setIndicator(createTab("消息", 2)).setContent(android.R.id.tabcontent));
+		tabHost.addTab(tabHost.newTabSpec("3").setIndicator(createTab("设置", 3)).setContent(android.R.id.tabcontent));
+		// 点击tabHost 来切换不同的消息
+		tabHost.setOnTabChangedListener(new OnTabChangeListener() {
 			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
-				intent.putExtra("click_url", "http://www.cnblogs.com/liqw/p/4181327.html");
-				startActivity(intent);
+			public void onTabChanged(String tabId) {
+				int index = Integer.parseInt(tabId);
+				setTabSelectedState(index, 4);
+				tabHost.getTabContentView().setVisibility(View.GONE);// 隐藏content
+			}
+		});
+		tabHost.setCurrentTab(0);
+
+		mfragment1 = new Fragment1();
+		mfragment2 = new Fragment2();
+		mfragment3 = new Fragment3();
+		mfragment4 = new Fragment4();
+
+		fragmentList = new ArrayList<Fragment>();
+		fragmentList.add(mfragment1);
+		fragmentList.add(mfragment2);
+		fragmentList.add(mfragment3);
+		fragmentList.add(mfragment4);
+
+		titleList.add("1");
+		titleList.add("2");
+		titleList.add("3");
+		titleList.add("4");
+
+		initJazzyPager(TransitionEffect.Standard);
+	}
+
+	/**
+	 * 动态创建 TabWidget
+	 * 的Tab项,并设置normalLayout的alpha为1，selectedLayout的alpha为0[显示normal，隐藏selected]
+	 * 
+	 * @param tabLabelText
+	 * @param tabIndex
+	 * @return
+	 */
+	private View createTab(String tabLabelText, int tabIndex) {
+		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.main_tabwidget_layout, null);
+		TextView normalTV = (TextView) tabIndicator.findViewById(R.id.normalTV);
+		TextView selectedTV = (TextView) tabIndicator.findViewById(R.id.selectedTV);
+		normalTV.setText(tabLabelText);
+		selectedTV.setText(tabLabelText);
+		ImageView normalImg = (ImageView) tabIndicator.findViewById(R.id.normalImg);
+		ImageView selectedImg = (ImageView) tabIndicator.findViewById(R.id.selectedImage);
+		switch (tabIndex) {
+		case 0:
+			normalImg.setImageResource(R.drawable.scan_book);
+			selectedImg.setImageResource(R.drawable.scan_book_hl);
+			break;
+		case 1:
+			normalImg.setImageResource(R.drawable.scan_qr);
+			selectedImg.setImageResource(R.drawable.scan_qr_hl);
+			break;
+		case 2:
+			normalImg.setImageResource(R.drawable.scan_street);
+			selectedImg.setImageResource(R.drawable.scan_street_hl);
+			break;
+		case 3:
+			normalImg.setImageResource(R.drawable.scan_word);
+			selectedImg.setImageResource(R.drawable.scan_word_hl);
+			break;
+		}
+		View normalLayout = tabIndicator.findViewById(R.id.normalLayout);
+		normalLayout.setAlpha(1f);// 透明度显示
+		View selectedLayout = tabIndicator.findViewById(R.id.selectedLayout);
+		selectedLayout.setAlpha(0f);// 透明的隐藏
+		Map<String, View> map = new HashMap<String, View>();
+		map.put("normal", normalLayout);
+		map.put("selected", selectedLayout);
+		tabViews.add(map);
+		return tabIndicator;
+	}
+
+	/**
+	 * 设置tab状态选中
+	 * 
+	 * @param index
+	 */
+	private void setTabSelectedState(int index, int tabCount) {
+		for (int i = 0; i < tabCount; i++) {
+			if (i == index) {
+				tabViews.get(i).get("normal").setAlpha(0f);
+				tabViews.get(i).get("selected").setAlpha(1f);
+			} else {
+				tabViews.get(i).get("normal").setAlpha(1f);
+				tabViews.get(i).get("selected").setAlpha(0f);
+			}
+		}
+		jazzyPager.setCurrentItem(index, false);// false表示 代码切换 pager
+												// 的时候不带scroll动画
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setTabSelectedState(tabHost.getCurrentTab(), 4);
+	}
+
+	private void initJazzyPager(TransitionEffect effect) {
+		tabHost.setCurrentTab(0);
+		jazzyPager.setTransitionEffect(effect);
+		jazzyPager.setAdapter(new MyViewPagerAdapter(getSupportFragmentManager()));
+		// jazzyPager.setAdapter(new MainAdapter());
+		jazzyPager.setPageMargin(0);
+		jazzyPager.setFadeEnabled(true);
+		jazzyPager.setSlideCallBack(new SlideCallback() {
+			@Override
+			public void callBack(int position, float positionOffset) {
+				// Log.i("", position+"  "+positionOffset);
+				Map<String, View> map = tabViews.get(position);
+				ViewHelper.setAlpha(map.get("selected"), positionOffset);
+				ViewHelper.setAlpha(map.get("normal"), 1 - positionOffset);
+			}
+		});
+		jazzyPager.setOnPageChangeListener(new OnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				tabHost.setCurrentTab(position);
+			}
+
+			@Override
+			public void onPageScrolled(int paramInt1, float paramFloat, int paramInt2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int paramInt) {
 			}
 		});
 	}
-	
-	
+
 	@Override
-	public void onBackPressed() {
-		super.onBackPressed();
-		Log.i(TAG, "finish");
-		System.exit(0);
-		finish();
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add("Toggle Fade");
+		String[] effects = this.getResources().getStringArray(R.array.jazzy_effects);
+		for (String effect : effects)
+			menu.add(effect);
+		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getTitle().toString().equals("Toggle Fade")) {
+			jazzyPager.setFadeEnabled(!jazzyPager.getFadeEnabled());
+		} else {
+			TransitionEffect effect = TransitionEffect.valueOf(item.getTitle().toString());
+			initJazzyPager(effect);
+		}
+		return true;
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration config) {
+		super.onConfigurationChanged(config);
+		
+		// 重新加载数据
+		initJazzyPager(TransitionEffect.Standard);
+	}
+
+	public class MyViewPagerAdapter extends FragmentPagerAdapter {
+		public MyViewPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			jazzyPager.setObjectForPosition(fragmentList.get(position), position);
+			return super.instantiateItem(container, position);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return fragmentList.get(position);
+		}
+
+		@Override
+		public int getCount() {
+			return fragmentList.size();
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return titleList.get(position);
+		}
+
+	}
+
+//	private class MainAdapter extends PagerAdapter {
+//		@Override
+//		public Object instantiateItem(ViewGroup container, final int position) {
+//			TextView text = new TextView(MainActivity.this);
+//			text.setGravity(Gravity.CENTER);
+//			text.setTextSize(30);
+//			text.setTextColor(Color.WHITE);
+//			text.setText("Page " + position);
+//			text.setPadding(30, 30, 30, 30);
+//			int bg = Color.rgb((int) Math.floor(Math.random() * 128) + 64, (int) Math.floor(Math.random() * 128) + 64, (int) Math.floor(Math.random() * 128) + 64);
+//			text.setBackgroundColor(bg);
+//			container.addView(text, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+//			jazzyPager.setObjectForPosition(text, position);
+//			return text;
+//		}
+//
+//		@Override
+//		public void destroyItem(ViewGroup container, int position, Object obj) {
+//			container.removeView(jazzyPager.findViewFromObject(position));
+//		}
+//
+//		@Override
+//		public int getCount() {
+//			return 4;
+//		}
+//
+//		@Override
+//		public boolean isViewFromObject(View view, Object obj) {
+//			if (view instanceof OutlineContainer) {
+//				return ((OutlineContainer) view).getChildAt(0) == obj;
+//			} else {
+//				return view == obj;
+//			}
+//		}
+//	}
+
 }
---
+++ src/com/example/mall/fragment/Fragment2.java
@@ -0,0 +1,74 @@
+package com.example.mall.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mall.R;
+
+public class Fragment2 extends Fragment{
+	private View mMainView;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		Log.v("huahua", "fragment2-->onCreate()");
+		
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		mMainView = inflater.inflate(R.layout.fragment2, (ViewGroup)getActivity().findViewById(R.id.jazzyPager), false);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		Log.v("huahua", "fragment2-->onCreateView()");
+		
+		ViewGroup p = (ViewGroup) mMainView.getParent(); 
+        if (p != null) { 
+            p.removeAllViewsInLayout(); 
+            Log.v("huahua", "fragment2-->�Ƴ��Ѵ��ڵ�View");
+        } 
+		
+		return mMainView;
+	}
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.v("huahua", "fragment2-->onDestroy()");
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.v("huahua", "fragment2-->onPause()");
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.v("huahua", "fragment2-->onResume()");
+	}
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.v("huahua", "fragment2-->onStart()");
+	}
+
+	@Override
+	public void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.v("huahua", "fragment2-->onStop()");
+	}
+
+}
