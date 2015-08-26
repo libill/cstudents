da5dac0ca4c374eb4eb59c85cddde64d9b1761b6
libill <770737185@qq.com>
2015-08-26 17:45:41 +0800

    add banner

---
+++ infinite-indicator/src/main/java/cn/lightsky/infiniteindicator/indicator/RecyleAdapter.java
@@ -0,0 +1,114 @@
+package cn.lightsky.infiniteindicator.indicator;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import cn.lightsky.infiniteindicator.jakewharton.salvage.RecyclingPagerAdapter;
+import cn.lightsky.infiniteindicator.slideview.BaseSliderView;
+
+public class RecyleAdapter extends RecyclingPagerAdapter implements BaseSliderView.BitmapLoadCallBack {
+
+    private Context mContext;
+    private LayoutInflater inflater;
+    private ArrayList<BaseSliderView> mSlederViews;
+    private boolean isLoop = true;
+    DataChangeListener mDataChangeListener;
+
+    public RecyleAdapter(Context context) {
+        mContext = context;
+        inflater = LayoutInflater.from(context);
+        mSlederViews = new ArrayList<BaseSliderView>();
+    }
+
+    public int getRealCount() {
+        return mSlederViews.size();
+    }
+
+    public <T extends BaseSliderView> void addSlider(T slider) {
+        slider.setOnImageLoadListener(this);
+        mSlederViews.add(slider);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * get really position
+     *
+     * @param position
+     * @return
+     */
+    public int getPosition(int position) {
+        return isLoop ? position % getRealCount() : position;
+    }
+
+    @Override
+    public int getCount() {
+        return isLoop ? getRealCount() * 100 : getRealCount();
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup container) {
+        return ((BaseSliderView) mSlederViews.get(getPosition(position))).getView();
+    }
+
+    public <T extends BaseSliderView> void removeSlider(T slider) {
+        if (mSlederViews.contains(slider)) {
+            mSlederViews.remove(slider);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void removeSliderAt(int position) {
+        if (mSlederViews.size() < position) {
+            mSlederViews.remove(position);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void removeAllSliders() {
+        mSlederViews.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoadStart(BaseSliderView target) {
+    }
+
+    @Override
+    public void onLoadComplete(BaseSliderView target) {
+    }
+
+    @Override
+    public void onLoadFail(BaseSliderView target) {
+
+        if (target.isShowErrorView() == true) {
+            return;
+        }
+
+        for (BaseSliderView slider : mSlederViews) {
+            if (slider.equals(target)) {
+                removeSlider(target);
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return the is Loop
+     */
+    public boolean isLoop() {
+        return isLoop;
+    }
+
+    /**
+     * @param isLoop the is InfiniteLoop to set
+     */
+    public void setLoop(boolean isLoop) {
+        this.isLoop = isLoop;
+        notifyDataSetChanged();
+    }
+
+}
--- sample/src/com/example/mall/fragment/Fragment1.java
+++ sample/src/com/example/mall/fragment/Fragment1.java
@@ -10,87 +10,118 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mall.R;
 import com.example.mall.activity.MainActivity;
 import com.example.mall.activity.WebViewActivity;
 
+import java.util.HashMap;
 
-public class Fragment1 extends Fragment{
-	private View mMainView;
-	private TextView tv;
-	private Button btn;
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		Log.v("huahua", "fragment1-->onCreate()");
-		
-		LayoutInflater inflater = getActivity().getLayoutInflater();
-		mMainView = inflater.inflate(R.layout.fragment1, (ViewGroup)getActivity().findViewById(R.id.jazzyPager), false);
-		
-		tv = (TextView)mMainView.findViewById(R.id.tv1);
-		btn = (Button)mMainView.findViewById(R.id.btn1);
-		btn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				tv.setText("Hello Viewpager\"");
-				Intent intent = new Intent(getActivity(), WebViewActivity.class);
-				intent.putExtra("click_url", "http://www.cnblogs.com/liqw/p/4181327.html");
-				startActivity(intent);
-			}
-		});
-		
-	}
+import cn.lightsky.infiniteindicator.InfiniteIndicatorLayout;
+import cn.lightsky.infiniteindicator.indicator.CircleIndicator;
+import cn.lightsky.infiniteindicator.slideview.BaseSliderView;
+import cn.lightsky.infiniteindicator.slideview.DefaultSliderView;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		Log.v("huahua", "fragment1-->onCreateView()");
-		
-		ViewGroup p = (ViewGroup) mMainView.getParent(); 
-        if (p != null) { 
-            p.removeAllViewsInLayout(); 
-            Log.v("huahua", "fragment1-->�Ƴ��Ѵ��ڵ�View");
-        } 
-		
-		return mMainView;
-	}
 
-	@Override
-	public void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		Log.v("huahua", "fragment1-->onDestroy()");
-	}
+public class Fragment1 extends Fragment implements BaseSliderView.OnSliderClickListener {
+    private View mMainView;
+    private TextView tv;
+    private Button btn;
+    private InfiniteIndicatorLayout mCustoemIndicatorLayout;
+    private HashMap<String, String> url_maps;
 
-	@Override
-	public void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		Log.v("huahua", "fragment1-->onPause()");
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        mMainView = inflater.inflate(R.layout.fragment1, (ViewGroup) getActivity().findViewById(R.id.jazzyPager), false);
 
-	@Override
-	public void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-		Log.v("huahua", "fragment1-->onResume()");
-	}
+        btn = (Button) mMainView.findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
 
-	@Override
-	public void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		Log.v("huahua", "fragment1-->onStart()");
-	}
+            @Override
+            public void onClick(View v) {
+                tv.setText("Hello Viewpager\"");
+                Intent intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra("click_url", "http://www.cnblogs.com/liqw/p/4181327.html");
+                startActivity(intent);
+            }
+        });
 
-	@Override
-	public void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-		Log.v("huahua", "fragment1-->onStop()");
-	}
+        url_maps = new HashMap<String, String>();
+        url_maps.put("Page A", "https://raw.githubusercontent.com/lightSky/InfiniteIndicator/master/res/a.jpg");
+        url_maps.put("Page B", "https://raw.githubusercontent.com/lightSky/InfiniteIndicator/master/res/b.jpg");
+        url_maps.put("Page C", "https://raw.githubusercontent.com/lightSky/InfiniteIndicator/master/res/c.jpg");
+        url_maps.put("Page D", "https://raw.githubusercontent.com/lightSky/InfiniteIndicator/master/res/d.jpg");
 
+        testCustomCircleIndicator();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup p = (ViewGroup) mMainView.getParent();
+        if (p != null) {
+            p.removeAllViewsInLayout();
+        }
+
+        return mMainView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mCustoemIndicatorLayout.stopAutoScroll();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCustoemIndicatorLayout.startAutoScroll();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    private void testCustomCircleIndicator() {
+        mCustoemIndicatorLayout = (InfiniteIndicatorLayout) mMainView.findViewById(R.id.indicator_custome_circle);
+        for (String name : url_maps.keySet()) {
+            DefaultSliderView textSliderView = new DefaultSliderView(getActivity());
+            textSliderView
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .showImageResForError(R.drawable.ic_launcher)
+                    .setOnSliderClickListener(this);
+            textSliderView.getBundle()
+                    .putString("extra", name);
+            mCustoemIndicatorLayout.addSlider(textSliderView);
+        }
+        mCustoemIndicatorLayout.setIndicatorPosition(InfiniteIndicatorLayout.IndicatorPosition.Center_Bottom);
+        CircleIndicator circleIndicator = ((CircleIndicator) mCustoemIndicatorLayout.getPagerIndicator());
+        final float density = getResources().getDisplayMetrics().density;
+        circleIndicator.setBackgroundColor(getResources().getColor(R.color.transparent));
+        circleIndicator.setRadius(5 * density);
+        circleIndicator.setPageColor(0x880000FF);
+        circleIndicator.setFillColor(0xFF888888);
+        circleIndicator.setStrokeColor(0xFF000000);
+        circleIndicator.setStrokeWidth(2 * density);
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(getActivity(), slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
+    }
 }
